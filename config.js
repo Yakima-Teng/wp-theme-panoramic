398f7b4ec59b621288884f0336a752b70fd4e4de
Yakima <cleveryun@163.com>
2017-10-20 19:42:56 +0800

    add script to enable sftp files directly through command line tools

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  publish: {
+    host: '111.11.111.11',
+    username: 'username',
+    password: 'password',
+    dest: '/var/www/html/orzzone.com/public_html/wp-content/themes/panoramic/'
+  },
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp');
 const scp = require('gulp-scp2');
+const config = require('./config');
 
 gulp.task('deploy', () => {
   return gulp.src('./panoramic/**/*.*')
     .pipe(scp({
-      host: '111.11.111.11',
-      username: 'username',
-      password: 'password',
-      dest: '/var/www/html/orzzone.com/public_html/wp-content/themes/panoramic/'
+      host: config.publish.host,
+      username: config.publish.username,
+      password: config.publish.password,
+      dest: config.publish.dest,
     }))
     .on('error', e => {
       console.log(e);
